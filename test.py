c01a815fa300761bea96a5ddfc22369cdee24ab4
E <75491816+TechnoShip123@users.noreply.github.com>
2022-01-20 00:46:33 +0000

    🧹 Added docstrings and type hints to test.py

--- test.py
+++ test.py
@@ -4,18 +4,27 @@ from main import Intents, chat
 
 class TestChatBot(unittest.TestCase):
     def test_age(self):
+        """
+        Tests if the bot's age is a non-negative number.
+        """
         print(f"Age is {Intents.age}.")
         self.assertTrue(Intents.age >= 0)
     
     def test_name(self):
+        """
+        Tests that the bot's name is a string ('str') variable type.
+        """
         print(f"Name is {Intents.name} and type {type(Intents.name)}.")
         self.assertEqual(type(Intents.name), str)
     
     def test_perf(self):
-        start_time = time.time()
+        """
+        Tests that the bot can complete 1000 responses in under 10 seconds.
+        """
+        start_time: float = time.time()
         for i in range(1000):
             chat.respond(str(i))
-        elapsed_time = time.time() - start_time
+        elapsed_time: float = time.time() - start_time
         print(f'Performance test took {elapsed_time.__round__(3)} seconds.')
         self.assertTrue(elapsed_time < 10.0)
 
